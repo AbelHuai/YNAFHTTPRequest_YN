be767b09e23869acd5db06b5318092ed4b03ae91
杨南 <15019429193@163.com>
2016-09-24 14:58:48 +0800

    更新

--- YNAFHTTPRequest/HTTPManager/UIViewController+HUD.h
+++ YNAFHTTPRequest/HTTPManager/UIViewController+HUD.h
@@ -1,15 +1,11 @@
-/************************************************************
-  *  * EaseMob CONFIDENTIAL 
-  * __________________ 
-  * Copyright (C) 2013-2014 EaseMob Technologies. All rights reserved. 
-  *  
-  * NOTICE: All information contained herein is, and remains 
-  * the property of EaseMob Technologies.
-  * Dissemination of this information or reproduction of this material 
-  * is strictly forbidden unless prior written permission is obtained
-  * from EaseMob Technologies.
-  */
 
+//
+//  UIViewController+HUD.h
+//  YNAFHTTPRequest
+//
+//  Created by Abel on 16/9/5.
+//  Copyright © 2016年 杨南. All rights reserved.
+//
 #import <UIKit/UIKit.h>
 
 @interface UIViewController (HUD)
